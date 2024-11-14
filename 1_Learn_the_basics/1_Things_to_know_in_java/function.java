a37d7bc8123819ce7517d0289134fce179eafe9a
Pratham Singh <prathamjii36@gmail.com>
2024-11-14 21:54:24 +0530

    learning time complexity

--- 1_Learn_the_basics/1_Things_to_know_in_java/function.java
+++ 1_Learn_the_basics/1_Things_to_know_in_java/function.java
@@ -9,14 +9,41 @@ import java.util.*;
 // parameterised
 // non parameterised
 
+// Take two numbers and print its sum
+
+
+// pass by value
+// pass by reference
+
 public class function{
-    public static void printName() {
-        System.out.println("hey Pratham Singh");
+    public static int sum(int num1, int num2) {
+        int num3 = num1 + num2;
+        return num3;
     }
+
+    public static int maxx(int num1, int num2){
+        if(num1 >= num2){
+            return num1;
+        }
+        else{
+            return num2;
+        }
+    }
+
+    public static void doSomething(String s) {
+        s = 't' + s.substring(0);
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        printName();
+        // int num1 = sc.nextInt();
+        // int num2 = sc.nextInt();
+        String s = "raj";
+        // int res = sum(num1, num2);
+        // int maximum = maxx(num1, num2);
+        doSomething(s);
+        System.out.println(s);
 
         sc.close();
 
---
+++ 1_Learn_the_basics/1_Things_to_know_in_java/timeComplexity.java
@@ -0,0 +1,13 @@
+public class timeComplexity {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+// time taken is not called time complexity
+
+// old windows taking - 2s
+// new macbook taking - 1s
+
+// The Rate of which the time taken increases with respect to the input size.
